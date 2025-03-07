66e920c92b584f36498cb1644dd95cb5747ba211
Ioannis Giannakaras <71703504+igianGH@users.noreply.github.com>
2025-03-07 15:35:37 +0200

    Update mathSL.py

--- mathSL.py
+++ mathSL.py
@@ -3,6 +3,19 @@ import random as rn
 from collections import defaultdict
 from datetime import date
 import time
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+matplotlib.rcParams.update({'font.size': 28})
+
+def Qu(line,fgs=(.1, .1),dpi=80):
+  plt.figure(figsize=fgs, dpi=dpi)
+  plt.grid(False)
+  plt.axis('off')
+  plt.text(.0,.0,r''+line)
+  plt.draw()
+  plt.show()
 
 def mquiz():
   Q,A=defaultdict(),defaultdict()
@@ -12,10 +25,10 @@ def mquiz():
     for line in f:
       nline+=1
       if(nline%2==1):
-        Q[nline//2+1]=line[:-3]
+        Q[nline//2+1]=line[:-3].replace('\t',' ')
         res=line[-2]
       else:
-        A[nline//2]=res,line[:-1]
+        A[nline//2]=res,line[:-1].replace('\t',' ')
   N=nline//2
   quiz=rn.sample(range(1,N+1),10)
   vathmos=0
@@ -23,7 +36,9 @@ def mquiz():
   start=time.time()
   for i in quiz:
     aa+=1
-    if(A[i][0]==input(str(aa)+'.'+Q[i][Q[i].find('.')+1:]+"\n> ")):
+    Qu(Q[i][Q[i].find('.')+1:])
+    if(A[i][0]==input("\n> "):
+    #if(A[i][0]==input(str(aa)+'.'+Q[i][Q[i].find('.')+1:]+"\n> ")):
       print("\nΣωστή Απάντηση!\n")
       vathmos+=10
     else:
