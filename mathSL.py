1fbd7f176f9049f34137167197e44f32314aff7c
Ioannis Giannakaras <71703504+igianGH@users.noreply.github.com>
2025-03-11 00:54:02 +0200

    Add files via upload

--- mathSL.py
+++ mathSL.py
@@ -7,9 +7,28 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-matplotlib.rcParams.update({'font.size': 28})
+from IPython.display import display as idisplay
+from IPython.display import Math as iMath
+from IPython.display import Latex as iLatex
+matplotlib.rcParams.update({'font.size': 25})
 
-def Qu(line,fgs=(.1, .1),dpi=80):
+def _Q2p(line, fgs=(28,.1),dpi=80):
+  #ND rt to plot plus
+  if(r"\ltx" not in line):
+    Q2p(line,fgs,dpi)
+  else:
+    pos1=line.find(r"\ltx")
+    pos2=line[pos1+4:].find(r"\ltx")+pos1+4
+    Q2p(line[:pos1],fgs,dpi)
+    print()
+    idisplay(iMath(r''+line[pos1:pos2+4].replace('\ltx','$')))
+    print()
+    _Q2p(line[pos2+4:],fgs,dpi)
+
+def Q2p(line,fgs=(28,.1),dpi=80):
+  if(line==''):
+    return
+  #1D rt to plot
   plt.figure(figsize=fgs, dpi=dpi)
   plt.grid(False)
   plt.axis('off')
@@ -23,26 +42,30 @@ def mquiz():
   with open("mtDB.txt",'r') as f:
     nline=0
     for line in f:
-      nline+=1
-      if(nline%2==1):
-        Q[nline//2+1]=line[:-3].replace('\t',' ')
-        res=line[-2]
+      line=line.replace('\t',' ')
+      if(len(line)<3):    # κενή γραμμή
+        continue
+      if(line[0] in "0123456789"):  #Question  ##if(nline%2==1):
+        nline+=1
+        Q[nline]=line[:-3]#.replace('\t',' ')
+        res=line[-2]  #Σ/Λ
       else:
-        A[nline//2]=res,line[:-1].replace('\t',' ')
-  N=nline//2
+        A[nline]=res,line[:-1]#.replace('\t',' ')
+  N=nline#//2
   quiz=rn.sample(range(1,N+1),10)
   vathmos=0
   aa=0
   start=time.time()
   for i in quiz:
     aa+=1
-    Qu(Q[i][Q[i].find('.')+1:])
-    if(A[i][0]==input("\n> ")):
-    #if(A[i][0]==input(str(aa)+'.'+Q[i][Q[i].find('.')+1:]+"\n> ")):
-      print("\nΣωστή Απάντηση!\n")
+    _Q2p(str(aa)+". "+Q[i][Q[i].find('.')+1:])
+    if(A[i][0]==input("\n> ")):#if(A[i][0]==input(str(aa)+'.'+Q[i][Q[i].find('.')+1:]+"\n> ")):
+      print("Σωστή Απάντηση!\n")
       vathmos+=10
     else:
-      print("\nΛάθος Απάντηση:\t"+A[i][1]+'\n')
+      print("Λάθος Απάντηση!")
+      Q2p("$\hookrightarrow$ "+A[i][1])
+      print("\n")
   print("\n______________________________________\nΗμερομηνία: "+time.strftime("%a, %d %b %Y, %H:%M:%S",time.gmtime(time.time())))
   end=time.time()
   print("\nΤελικός Βαθμός:",vathmos,"/ 100\nΧρόνος:",int(end-start),"seconds")
