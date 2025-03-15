ffb32ee040ed99bb0a4b4a3cc03f87989a23f578
Ioannis Giannakaras <71703504+igianGH@users.noreply.github.com>
2025-03-15 23:14:37 +0200

    Add files via upload

--- mathSL.py
+++ mathSL.py
@@ -12,7 +12,7 @@ from IPython.display import Math as iMath
 from IPython.display import Latex as iLatex
 matplotlib.rcParams.update({'font.size': 25})
 
-def _Q2p(line, fgs=(28,.1),dpi=80):
+def _Q2p(line, fgs=(22,.1),dpi=80):
   #ND rt to plot plus
   if(r"\ltx" not in line):
     Q2p(line,fgs,dpi)
@@ -25,7 +25,7 @@ def _Q2p(line, fgs=(28,.1),dpi=80):
     print()
     _Q2p(line[pos2+4:],fgs,dpi)
 
-def Q2p(line,fgs=(28,.1),dpi=80):
+def Q2p(line,fgs=(22,.1),dpi=80):
   if(line==''):
     return
   #1D rt to plot
@@ -36,7 +36,7 @@ def Q2p(line,fgs=(28,.1),dpi=80):
   plt.draw()
   plt.show()
 
-def mquiz():
+def mquiz(M=10,rquiz=True,frange=(1,2)):
   Q,A=defaultdict(),defaultdict()
   line=r"""$\bullet$ Να απαντήσετε με Σ/Λ στις παρακάτω ερωτήσεις:
   """
@@ -51,12 +51,21 @@ def mquiz():
         nline+=1
         Q[nline]=line[:-3]#.replace('\t',' ')
         res=line[-2]  #Σ/Λ
+        A[nline]=[res,r'']
       else:
-        A[nline]=res,line[:-1]#.replace('\t',' ')
+        A[nline][1]+='\n'+line[:-1]#.replace('\t',' ')
   N=nline#//2
-  quiz=rn.sample(range(1,N+1),10)
+  if(M>N or M<1):
+    M=10
+  if(frange[0]<1 or frange[0]>=frange[1] or frange[1]>N+1):
+    frange=[1,N+1]
+  quiz=range(frange[0],frange[1])
+  step=100/(frange[1]-frange[0])
+  if(rquiz):
+    quiz=rn.sample(range(1,N+1),M)
+    step=100/M
   vathmos=0
-  aa=0
+  aa=frange[0]-1
   start=time.time()
   for i in quiz:
     aa+=1
@@ -64,13 +73,15 @@ def mquiz():
     apantisi=input("\n> ")
     if(A[i][0]==apantisi):#if(A[i][0]==input(str(aa)+'.'+Q[i][Q[i].find('.')+1:]+"\n> ")):
       print("Σωστή Απάντηση!\n")
-      vathmos+=10
-    elif(apantisi in ['telos','τελος']):
+      _Q2p("$\hookrightarrow$ "+A[i][1])
+      print("\n")
+      vathmos+=step
+    elif(apantisi in ['telos','τελος','τελοσ']):
       break
     else:
       print("Λάθος Απάντηση!")
-      Q2p("$\hookrightarrow$ "+A[i][1])
+      _Q2p("$\hookrightarrow$ "+A[i][1])
       print("\n")
   print("\n______________________________________\nΗμερομηνία: "+time.strftime("%a, %d %b %Y, %H:%M:%S",time.gmtime(time.time())))
   end=time.time()
-  print("\nΤελικός Βαθμός:",vathmos,"/ 100\nΧρόνος:",int(end-start),"seconds")
+  print("\nΤελικός Βαθμός:",int(vathmos*10)/10,"/ 100.0\nΧρόνος:",int(end-start),"seconds")
